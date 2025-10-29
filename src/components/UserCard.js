f09f6550eb8d68d5184f95c77c79dbde15c016a6
Marysah <Marysah2@gmail.com>
2025-10-29 23:53:11 +0300

    initial commit

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,12 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useOutletContext } from "react-router-dom";
 
-function UserCard({user}) {
-    return (
-      <article>
-          <h2>{user.name}</h2>
-          <Link to={`/profile/${user.id}`}>View profile</Link>
-      </article>
-    );
-  };
-  
-  export default UserCard;
+function UserCard({ user }) {
+  // Demo: you can read the full users array here if you ever need it
+  const allUsers = useOutletContext();
+  console.log("All users in UserCard:", allUsers);
+
+  return (
+    <article>
+      <h2>{user.name}</h2>
+      <p>
+        <Link to={`/profile/${user.id}`}>View profile</Link>
+      </p>
+    </article>
+  );
+}
+
+export default UserCard;
